b2c3ac1dab34dc14cb950a1100f20a1dbdbfb16e
C0ng_yun <s25069@gsm.hs.kr>
2026-01-20 23:30:09 +0900

    add : MemberEntity 구현

---
+++ src/main/java/WAY/way/domain/member/entity/MemberEntity.java
@@ -0,0 +1,39 @@
+package WAY.way.domain.member.entity;
+
+import WAY.way.domain.member.presentation.data.Role;
+import WAY.way.domain.reservation.entity.ReservationEntity;
+import WAY.way.domain.room.entity.RoomEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "member")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservationId", nullable = false)
+    private ReservationEntity reservation_id;
+
+    @Column(nullable = false, unique = true ,name = "email")
+    private String email;
+
+    @Column(nullable = false, unique = true ,name = "name")
+    private String name;
+
+    @Column(nullable = false, unique = true ,name = "student_number", length =4)
+    private String studentNumber;
+
+    @Column(nullable = false, name = "role")
+    private Role role;
+}
