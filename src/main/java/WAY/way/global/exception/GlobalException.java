0cb8d864c60b1c2299435216077a5ab9b1d06064
C0ng_yun <s25069@gsm.hs.kr>
2026-02-04 17:46:01 +0900

    add : GlobalException 구현

---
+++ src/main/java/WAY/way/global/exception/GlobalException.java
@@ -0,0 +1,10 @@
+package WAY.way.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GlobalException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
