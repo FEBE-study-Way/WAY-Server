d3823a2a37777020a548b038417743aab7cbb927
C0ng_yun <s25069@gsm.hs.kr>
2026-02-04 17:48:33 +0900

    update : @ConfigurationPropertiesScan 추가

--- src/main/java/WAY/way/WayApplication.java
+++ src/main/java/WAY/way/WayApplication.java
@@ -2,8 +2,10 @@ package WAY.way;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class WayApplication {
 
 	public static void main(String[] args) {
