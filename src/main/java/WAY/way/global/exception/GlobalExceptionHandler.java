32cd2925bd37bfecb8ad3c796a3cd43cf1ac2237
C0ng_yun <s25069@gsm.hs.kr>
2026-02-04 17:46:11 +0900

    add : GlobalExceptionHandler 구현

---
+++ src/main/java/WAY/way/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package WAY.way.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(GlobalException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(e.getErrorCode().getStatus())
+                .message(e.getErrorCode().getMessage())
+                .build();
+
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("처리되지 않은 예외", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(500, e.getMessage()));
+    }
+}
