c8de23cdd947a340dd905499f634b06bc818ff1c
C0ng_yun <s25069@gsm.hs.kr>
2026-02-04 17:47:45 +0900

    add : MemberRepository 구현

---
+++ src/main/java/WAY/way/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package WAY.way.domain.member.repository;
+
+import WAY.way.domain.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String email);
+}
