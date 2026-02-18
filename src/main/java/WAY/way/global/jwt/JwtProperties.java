5d6e9dc076474da9249dee8f48da136f446159dc
C0ng_yun <s25069@gsm.hs.kr>
2026-02-18 20:45:26 +0900

    update: 토큰 만료일 변수명 변경

--- src/main/java/WAY/way/global/jwt/JwtProperties.java
+++ src/main/java/WAY/way/global/jwt/JwtProperties.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Date;
+
 @Getter
 @AllArgsConstructor
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private final String secret;
-    private final long accessTokenValidity;
-    private final long refreshTokenValidity;
+    private final long accessTokenExpiration;
+    private final long refreshTokenExpiration;;
 }
