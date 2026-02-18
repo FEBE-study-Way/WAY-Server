387bcd3367a60d15694ea8d94b170d69cf72cf7b
C0ng_yun <s25069@gsm.hs.kr>
2026-02-18 20:56:35 +0900

    add : RefreshTokenService 추가

---
+++ src/main/java/WAY/way/domain/auth/service/impl/RefreshTokenServiceImpl.java
@@ -0,0 +1,70 @@
+package WAY.way.domain.auth.service.impl;
+
+import WAY.way.domain.auth.entity.RefreshToken;
+import WAY.way.domain.auth.exception.InvalidRefreshToken;
+import WAY.way.domain.auth.exception.RefreshTokenNNotFound;
+import WAY.way.domain.auth.presentation.data.response.TokenResponse;
+import WAY.way.domain.auth.repository.RefreshTokenRepository;
+import WAY.way.domain.auth.service.RefreshTokenService;
+import WAY.way.domain.member.presentation.data.Role;
+import WAY.way.global.jwt.JwtProperties;
+import WAY.way.global.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtProvider jwtProvider;
+    private final JwtProperties jwtProperties;
+
+    @Override
+    @Transactional
+    public TokenResponse execute(Long userId, String email, Role role) {
+        TokenResponse response = jwtProvider.receiveToken(userId, email, role);
+
+        refreshTokenRepository.deleteById(userId);
+
+        RefreshToken token = RefreshToken.builder()
+                .id(userId)
+                .token(response.refreshToken())
+                .expiresIn(jwtProperties.getRefreshTokenExpiration())
+                .build();
+
+        refreshTokenRepository.save(token);
+        return response;
+    }
+
+    @Override
+    @Transactional
+    public TokenResponse reissueAccessToken(String refreshToken) {
+        jwtProvider.validateToken(refreshToken);
+
+        Long userId = jwtProvider.getUserId(refreshToken);
+        String email = jwtProvider.getUserEmail(refreshToken);
+        Role role = jwtProvider.getRole(refreshToken);
+
+
+
+        RefreshToken stored = refreshTokenRepository.findById(userId)
+                .orElseThrow(RefreshTokenNNotFound::new);
+        // 로그 추가: DB 값과 들어온 값 비교
+        System.out.println("DB 토큰: " + stored.getToken());
+        System.out.println("보낸 토큰: " + refreshToken);
+
+        if(!stored.getToken().equals(refreshToken)) {
+            throw new InvalidRefreshToken();
+        }
+
+        TokenResponse response = jwtProvider.receiveToken(userId, email, role);
+
+        stored.updateToken(response.refreshToken());
+
+        return response;
+    }
+}
