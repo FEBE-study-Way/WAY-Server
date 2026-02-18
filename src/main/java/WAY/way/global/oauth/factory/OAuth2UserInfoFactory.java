42ef5d7f75b33c0c52cf1f62bd95b65473b05177
C0ng_yun <s25069@gsm.hs.kr>
2026-02-18 20:57:21 +0900

    update :  registrationId 추가

--- src/main/java/WAY/way/global/oauth/factory/OAuth2UserInfoFactory.java
+++ src/main/java/WAY/way/global/oauth/factory/OAuth2UserInfoFactory.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(
+            String registrationId,
             Map<String, Object> attributes) {
 
         return new GoogleOAuth2UserInfo(attributes);
