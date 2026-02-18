7597d0591ab829fa950603a94273c37346db5d23
C0ng_yun <s25069@gsm.hs.kr>
2026-02-18 20:54:32 +0900

    add : OAuthProviderConfig 추가

---
+++ src/main/java/WAY/way/global/oauth/config/OAuthProviderConfig.java
@@ -0,0 +1,41 @@
+package WAY.way.global.oauth.config;
+
+import WAY.way.global.oauth.common.OAuthType;
+import WAY.way.global.oauth.data.ProviderProperties;
+import WAY.way.global.oauth.exception.UnsupportedOAuthProviderException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class OAuthProviderConfig {
+
+    private final Map<OAuthType, ProviderProperties> providers;
+
+    public OAuthProviderConfig(
+            @Value("${spring.security.oauth2.client.registration.google.client-id}") String googleClientId,
+            @Value("${spring.security.oauth2.client.registration.google.client-secret}") String googleClientSecret,
+            @Value("${spring.security.oauth2.client.provider.google.token-uri}") String googleTokenUri,
+            @Value("${spring.security.oauth2.client.provider.google.user-info-uri}") String googleUserInfoUri,
+            @Value("${spring.security.oauth2.client.registration.google.redirect-uri}") String googleRedirectUri
+            ){
+
+        providers  = Map.of(
+                OAuthType.GOOGLE, new ProviderProperties(
+                        googleClientId, googleClientSecret, googleTokenUri, googleUserInfoUri, googleRedirectUri
+                )
+        );
+    }
+
+    public ProviderProperties get(OAuthType oAuthType){
+
+        ProviderProperties providerProperties = providers.get(oAuthType);
+
+        if(providerProperties == null){
+            throw new UnsupportedOAuthProviderException();
+        }
+        return providerProperties;
+    }
+
+}
