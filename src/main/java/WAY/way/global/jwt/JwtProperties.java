772b308575c4046d9624b1a41d212efc3322228b
C0ng_yun <s25069@gsm.hs.kr>
2026-02-18 21:06:10 +0900

    update : 중북 세미콜론 제거

--- src/main/java/WAY/way/global/jwt/JwtProperties.java
+++ src/main/java/WAY/way/global/jwt/JwtProperties.java
@@ -12,5 +12,5 @@ import java.util.Date;
 public class JwtProperties {
     private final String secret;
     private final long accessTokenExpiration;
-    private final long refreshTokenExpiration;;
+    private final long refreshTokenExpiration;
 }
