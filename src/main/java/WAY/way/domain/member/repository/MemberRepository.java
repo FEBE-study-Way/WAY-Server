fcbb532f82186670ca3e70b076a5f10eb9f003ad
C0ng_yun <s25069@gsm.hs.kr>
2026-02-21 21:55:43 +0900

    update : role 기준 조회 메서드 추가

--- src/main/java/WAY/way/domain/member/repository/MemberRepository.java
+++ src/main/java/WAY/way/domain/member/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package WAY.way.domain.member.repository;
 
 import WAY.way.domain.member.entity.MemberEntity;
+import WAY.way.domain.member.presentation.data.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByEmail(String email);
+    List<MemberEntity> findAllByRole(Role role);
 }
