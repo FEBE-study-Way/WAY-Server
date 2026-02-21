1e2bd571cfaacc3d1a830517c6e3b0f6592767df
C0ng_yun <s25069@gsm.hs.kr>
2026-02-21 22:41:00 +0900

    update: MemberUtil 구조 리펙토링

--- src/main/java/WAY/way/global/util/MemberUtil.java
+++ src/main/java/WAY/way/global/util/MemberUtil.java
@@ -6,10 +6,11 @@ import WAY.way.domain.member.exception.NotFoundMemberException;
 import WAY.way.domain.member.repository.MemberRepository;
 import WAY.way.global.auth.MemberDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -18,13 +19,12 @@ public class MemberUtil {
     private final MemberRepository memberRepository;
 
     public MemberEntity getCurrentMember() {
-        Object principal = Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication()).getPrincipal();
-
-        if (principal instanceof MemberDetails memberDetails) {
-            String email = memberDetails.getUsername();
-            return memberRepository.findByEmail(email)
-                    .orElseThrow(NotFoundMemberException::new);
-        }
-        throw new UnauthorizedUserException();
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof MemberDetails)
+                .map(principal -> (MemberDetails) principal)
+                .map(MemberDetails::getUsername)
+                .flatMap(memberRepository::findByEmail)
+                .orElseThrow(UnauthorizedUserException::new);
     }
 }
